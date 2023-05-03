5b2d2c981c53b110c3bfc16009aea0ad1aac6b1e
helloobaby <84119280+helloobaby@users.noreply.github.com>
2023-05-03 21:38:56 +0800

    Create GetProcessIdByName.cpp

---
+++ CorC++/GetProcessIdByName.cpp
@@ -0,0 +1,19 @@
+#include <TlHelp32.h>
+DWORD GetProcessIdByName(const wchar_t *name) {
+	PROCESSENTRY32 entry;
+	entry.dwSize = sizeof(PROCESSENTRY32);
+
+	HANDLE snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, NULL);
+
+	if (Process32First(snapshot, &entry) == TRUE) {
+		while (Process32Next(snapshot, &entry) == TRUE) {
+			if (_wcsicmp(entry.szExeFile, name) == 0) {
+				CloseHandle(snapshot); //thanks to Pvt Comfy
+				return entry.th32ProcessID;
+			}
+		}
+	}
+
+	CloseHandle(snapshot);
+	return 0;
+}
